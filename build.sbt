06e76ee6a9a73041b2daae83a9449f38eadc39ba
Eugene Yokota <eed3si9n@gmail.com>
2013-11-29 20:12:40 -0500

    wip

--- build.sbt
+++ build.sbt
@@ -50,10 +50,10 @@ numberTask := {
 
 val foo: Def.Initialize[Task[Int]] = {
   var n: Int = 0
-  val t1 = Def.task { startServer.value }
-  val t2 = Def.taskDyn { val _ = t1.value; Def.task { n = numberTask.value } }
-  val t3 = Def.taskDyn { val _ = t2.value; Def.task { Thread.sleep(1000) } }
-  val t4 = Def.taskDyn { val _ = t3.value; Def.task { val x = stopServer.value } }
+  val t1 = Def.task { startServer.value; () }
+  val t2 = Def.taskDyn { val _ = t1.value; Def.task { n = numberTask.value; () } }
+  val t3 = Def.taskDyn { val _ = t2.value; Def.task { Thread.sleep(1000); () } }
+  val t4 = Def.taskDyn { val _ = t3.value; Def.task { val x = stopServer.value; () } }
   Def.taskDyn { val _ = t4.value; Def.task { n } }
 }
 
--- src/main/scala/Plugin.scala
+++ src/main/scala/Plugin.scala
@@ -27,25 +27,32 @@ object CustomTaskMacro {
     val nameCache: mutable.Map[String, String] = mutable.Map()
     def freshNames(key: String): String = nameCache getOrElseUpdate (key, c.fresh()) 
     def taskName(idx: Int): String = freshNames("task" + idx.toString)
+    def VAL(name: String)(rhs: => Tree): Tree = ValDef(NoMods, name, TypeTree(), rhs)
     def wrapInTask(tree: Tree): Expr[Def.Initialize[Task[T]]] =
       reify(Def.task { c.Expr[T](tree).splice })
+    def wrapInTaskWithUnit(tree: Tree): Expr[Def.Initialize[Task[Unit]]] =
+      reify(Def.task {
+        c.Expr[Unit](tree).splice
+        ()
+      })
     def declareDummyVal(prev: String): Tree =
-      ValDef(NoMods, "_", TypeTree(), Select(Ident(newTermName(prev)), newTermName("value")))
+      VAL("_") (Select(Ident(newTermName(prev)), newTermName("value")))
     def declareValWrapInTask(name: String, body: Tree): Tree =
-      ValDef(NoMods, name, TypeTree(), wrapInTask(body).tree)      
+      VAL(name) (wrapInTaskWithUnit(moveValOut(body)).tree)
     def declareValWrapInTaskDyn(name: String, body: Tree, prev: String): Tree =
-      ValDef(NoMods, name, TypeTree(), reify(Def.taskDyn {
+      VAL(name) (reify(Def.taskDyn {
         c.Expr[Def.Initialize[Task[Unit]]](Block(
           List(declareDummyVal(prev)),
-          wrapInTask(body).tree
+          wrapInTaskWithUnit(moveValOut(body)).tree
         )).splice
       }).tree)
+    def moveValOut(body: Tree): Tree = body
     def emulateFlatMaps(trees0: Vector[Tree], tree0: Tree): Expr[Def.Initialize[Task[T]]] = {
-      val transformed = trees0.zipWithIndex map { case (t, idx) =>
+      val taskDyns = trees0.zipWithIndex map { case (t, idx) =>
         if (idx == 0) declareValWrapInTask(taskName(0), t)
         else declareValWrapInTaskDyn(taskName(idx), t, taskName(idx - 1))
       }
-      buff ++= transformed
+      buff ++= taskDyns
       c.Expr[Def.Initialize[Task[T]]](Block(
         buff.toList,
         reify(Def.taskDyn {
